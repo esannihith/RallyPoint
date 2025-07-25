5bbe774b1d77473a5c017d3438725fa567c7253a
esannihith <esanni04@gmail.com>
2025-07-25 10:59:51 +0530

    Build trail 2

--- app.config.js
+++ app.config.js
@@ -51,9 +51,9 @@ export default {
 
     extra: {
       // Added EXPO_PUBLIC_ prefix for client-side variables
-      EXPO_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-      EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN,
-      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+      GOOGLE_MAPS_API_KEY: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      MAPBOX_ACCESS_TOKEN: process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN,
+      API_URL: process.env.EXPO_PUBLIC_API_URL,
       "eas": {
         "projectId": "f7fd7692-9e75-45e3-9d87-95022a4bbf09"
       }
